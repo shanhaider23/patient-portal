9c3b330c65f9bac5b9a40def8e416f2733e95982
shan-e-haider bukhari <shanhaider23@lenovo-home.localdomain>
2025-08-13 16:20:47 +0200

    added docker compose

--- frontend/app/patients/page.tsx
+++ frontend/app/patients/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import api from "@/lib/api";
 import { useAuth } from "../context/AuthContext";
+import Image from "next/image";
 import {
     Calendar,
     User,
@@ -154,13 +155,16 @@ export default function PatientsPage() {
         `}
                     >
                         <div className="flex flex-col items-center">
-                            <img
+                            <Image
                                 src={
                                     selectedPatient.photoUrl ||
                                     `https://ui-avatars.com/api/?name=${selectedPatient.firstName}+${selectedPatient.lastName}&background=random`
                                 }
                                 alt="Profile"
-                                className={`${sidebarOpen ? "w-32 h-32" : "w-12 h-12"} rounded-full object-cover transition-all duration-300`}
+                                width={sidebarOpen ? 128 : 48}
+                                height={sidebarOpen ? 128 : 48}
+                                className="rounded-full object-cover transition-all duration-300"
+                                priority
                             />
                             {sidebarOpen && (
                                 <>
